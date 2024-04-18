af5d9309f96315515c307ab28630a3b9c9750996
Bita121 <francu352000@gmail.com>
2024-04-18 21:38:57 +0300

    dada

--- ExpensesPage.xaml.cs
+++ ExpensesPage.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Dollar_Wise.Models;
 using Dollar_Wise.Services;
 using Microsoft.Maui.Controls;
@@ -10,34 +11,106 @@ namespace Dollar_Wise
     public partial class ExpensesPage : ContentPage, INotifyPropertyChanged
     {
         private DataService _dataService;
+        private List<Expense> _allExpenses;
+        private bool _isDateFilterVisible = false;
+        private bool _isCategoryFilterVisible = false;
 
         public ExpensesPage()
         {
             InitializeComponent();
             _dataService = new DataService(App.Database);
             LoadExpenses();
+            FilterPicker.ItemsSource = new List<string> { "Date", "Category"};
+            CategoryPicker.ItemsSource = new List<string> { "Food", "Transportation", "Entertainment", "Utilities", "Investments", "Other" };
         }
 
         private async void LoadExpenses()
         {
             // Await the asynchronous operation to get expenses
-            var expenses = await _dataService.GetExpenses();
+            _allExpenses = await _dataService.GetExpenses();
 
             // Set the ItemsSource property with the result
-            ExpensesListView.ItemsSource = expenses;
+            ExpensesListView.ItemsSource = _allExpenses;
         }
+        
 
-        private async void AddExpense_Tapped(object sender, EventArgs e)
+        private async void FilterPicker_SelectedIndexChanged(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new ExpenseDialogPage());
-            LoadExpenses();
+            string selectedFilter = FilterPicker.SelectedItem as string;
+            if (selectedFilter == "Date")
+            {
+                _isDateFilterVisible = true;
+                _isCategoryFilterVisible = false;
+            }
+            else if (selectedFilter == "Category")
+            {
+                _isDateFilterVisible = false;
+                _isCategoryFilterVisible = true;
+            }
+            else
+            {
+                _isDateFilterVisible = false;
+                _isCategoryFilterVisible = false;
+            }
+
+            UpdateFilterVisibility();
         }
 
+        private void UpdateFilterVisibility()
+        {
+            DateFilterLayout.IsVisible = _isDateFilterVisible;
+            CategoryPicker.IsVisible = _isCategoryFilterVisible;
+            ApplyFilterButton.IsVisible = _isDateFilterVisible || _isCategoryFilterVisible;
+            ResetFilterButton.IsVisible = _isDateFilterVisible || _isCategoryFilterVisible;
+        }
         protected override void OnAppearing()
         {
             base.OnAppearing();
 
             // Reload expenses when the ExpensesPage is shown again
+            LoadExpenses();
+        }
+
+        private async void ApplyFilter_Clicked(object sender, EventArgs e)
+        {
+            // Get selected category and date range
+            string selectedCategory = CategoryPicker.SelectedItem as string;
+            DateTime startDate = StartDatePicker.Date;
+            DateTime endDate = EndDatePicker.Date;
+
+            // Apply filters
+            IEnumerable<Expense> filteredExpenses = _allExpenses;
+            if (_isCategoryFilterVisible && selectedCategory != null)
+            {
+                filteredExpenses = filteredExpenses.Where(expense => expense.Category == selectedCategory);
+            }
+            if (_isDateFilterVisible)
+            {
+                filteredExpenses = filteredExpenses.Where(expense => expense.Date >= startDate && expense.Date <= endDate);
+            }
+
+            // Update ListView with filtered data
+            ExpensesListView.ItemsSource = filteredExpenses;
+        }
+
+        private async void ResetFilter_Clicked(object sender, EventArgs e)
+        {
+            // Reset filter options
+            FilterPicker.SelectedItem = null;
+            CategoryPicker.SelectedItem = null;
+            StartDatePicker.Date = DateTime.Today;
+            EndDatePicker.Date = DateTime.Today;
+            _isDateFilterVisible = false;
+            _isCategoryFilterVisible = false;
+            UpdateFilterVisibility();
+
+            // Reload all expenses
+            LoadExpenses();
+        }
+
+        private async void AddExpense_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ExpenseDialogPage());
             LoadExpenses();
         }
 
@@ -51,7 +124,6 @@ namespace Dollar_Wise
                 await Navigation.PushAsync(new ExpenseDialogPageEdit(expense));
             }
         }
-
 
         private async void DeleteExpense_Clicked(object sender, EventArgs e)
         {
